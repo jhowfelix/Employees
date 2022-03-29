242c7b627b1f2a163162cf3fe19721fda4b20d3b
Jonathan Felix <jhow.wi56@gmail.com>
2022-03-29 12:22:05 -0300

    first commit

---
+++ src/main/java/br/com/vertigo/CrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package br.com.vertigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeApplication.class, args);
+	}
+
+}
